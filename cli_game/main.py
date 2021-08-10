b30e82bdcdd3e07049d0f0c09f2b8461a56c126e
ulhangry <80541020+umjiwan@users.noreply.github.com>
2021-08-10 22:50:35 +0900

    cli_game

---
+++ cli_game/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+
+def play(turn):
+    while run:
+        if run == 12:
+            print(303030)
+        else:
+            os.system("clear")
+            
+            print(omok_board)
+
+            if turn % 2 == 1:
+                turn_type = "black"
+            else:
+                turn_type = "white"
+        
+            print("")
+            print(turn_type)
+            
+            row_point = int(input("row : "))
+            column_point = int(input("column : "))
+            print(f"{row_point}, {column_point}")
+        
+            print("")
+            
+            if omok_board[row_point, column_point] != 0:
+                print("nope")
+            else:
+                if turn_type == "black":
+                    omok_board[row_point - 1, column_point - 1] = black
+                elif turn_type == "white":
+                    omok_board[row_point - 1, column_point - 1] = white
+
+                turn += 1
+
+size = 15
+omok_board = np.zeros([size, size])
+omok_overlap = 0
+black = 1
+white = 2
+
+first_turn = 1
+run = True
+
+play(first_turn)
+
