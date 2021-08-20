b028ce2350a1646dd84894f8470df9a08c9ca099
ulhangry <80541020+umjiwan@users.noreply.github.com>
2021-08-20 22:34:35 +0900

    update_omok

--- gui_game/main.py
+++ gui_game/main.py
@@ -7,23 +7,9 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 pygame.display.set_caption("ul omok")
 
-
 oo = 53
 spot_pos = 50/2
 block = 50
-running = True
-pos_count = -1
-
-while running:
-    apb = pos.pos_apb(pos_count)
-    
-    for i in range(15):
-        globals()[f"{apb}{i}_pos"] = [-53, -53]
-    # print(globals()[f"{apb}{i}_pos"])
-    pos_count += 1
-    
-    if pos_count > 13:
-        running = False
 
 running = True
 pos_count = -1
@@ -34,13 +20,28 @@ while running:
     for i in range(15):
         globals()[f"{apb}{i}"] = [oo + (i * 50), (oo + block*(pos_count+1))]
     pos_count += 1
+
+    if pos_count > 13:
+        running = False
+
+running = True
+pos_count = -1
+
+while running:
+    apb = pos.pos_apb(pos_count)
+    
+    for i in range(15):
+        globals()[f"{apb}{i}_pos"] = [(oo + (i * 50)) * -1, ((oo + block*(pos_count+1)) * -1 )]
+    # print(globals()[f"{apb}{i}_pos"])
+    pos_count += 1
+    
     if pos_count > 13:
         running = False
 
 run = True
 
 background = pygame.image.load("src/img/omok_board.png") # omok_board
-background = pygame.transform.scale(background, (700, 700))
+background = pygame.transform.scale(background, (700, 700)) # board_scale
 #load
 omok_black = pygame.image.load("src/img/omok_black.png")
 omok_white = pygame.image.load("src/img/omok_white.png")
